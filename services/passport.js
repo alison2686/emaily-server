a90b5720f3ec33ddda299456a4dc40ef10968f3f
Alison Lee <alison2686@gmail.com>
2023-10-17 16:16:08 -0700

    deserialize user

--- services/passport.js
+++ services/passport.js
@@ -10,6 +10,12 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 // creates a new instance of GoogleStrategy, pass in configuration
 passport.use(
   new GoogleStrategy(
