6b17b4791e7a776f0dacc4b3c93804aae5b2e368
Alison Lee <alison2686@gmail.com>
2023-10-17 18:32:23 -0700

    authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,8 @@ module.exports = (app) => {
 
   // route handler for google auth callback
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
