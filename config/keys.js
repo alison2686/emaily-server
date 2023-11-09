731daaeade7957effea0c4bbcc9621f041161922
Alison Lee <alison2686@gmail.com>
2023-11-09 15:45:40 -0800

    add prod.js

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// jeys.js - figure out what set of credentials to return
+
+if (process.env.NODE_ENV === 'production') {
+  // we are in prod return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in dev return the dev keys
+  module.expoers = require('./dev');
+}
